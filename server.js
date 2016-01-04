94c708a7d7df27a5cc78fc26d2d27a04c3424664
Chaz <cmtlum73@gmail.com>
2016-01-04 12:12:55 -1000

    parsed buffer to isolate file requested

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var net = require( 'net' );
 
-var server = net.createServer(function (socket) {
-  socket.end("get outta here you!\n");
-});
+var server = net.createServer( userConnection );
+
+function userConnection( socketReq ) {
+  console.log( 'I feel a little tickle!\n' );
+  socketReq.setEncoding( 'utf-8' );
+
+  socketReq.on( 'data', function( buffer ) {
+    console.log( 'socket DATA event, fire in the hole!\n' );
+    var parsedBuffer = buffer.split( ' ' );
+
+    console.log( parsedBuffer[1] );
+  });
+
+
+  socketReq.end("get outta here you!\n");
+}
 
 server.listen( { port: 8080 }, function() {
   address = server.address();
